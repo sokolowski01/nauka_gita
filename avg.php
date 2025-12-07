eea43f1a7600ec6c9d32a8357a407c3b2cdbbcc5
sokolowski01 <97532@student.wroclaw.merito.pl>
2025-12-07 15:28:33 +0100

    srednia wazona

--- avg.php
+++ avg.php
@@ -1,8 +1,19 @@
 <?php
 
-function srednia(&liczby) {
-	if (count(&liczby) === 0) {
+function srednia($liczby) {
+	if (count($liczby) === 0) {
 		return 0;
 	}
-	return array_sum(&liczby) / count(&liczby);
+	return array_sum($liczby) / count($liczby);
+}
+
+	$suma_wazona = 0;
+	$suma_wag = 0;
+
+	for ($i = 0; $i < count($wartosci); $i++) {
+		$suma_wazona += $wartosci[&i] * $wagi[$i];
+		$suma_wag += $wagi[$i];
+}
+
+	return $suma_wag === 0 ? 0 : $suma_wazona / $suma_wag;
 }
