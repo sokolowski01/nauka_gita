2f54ea4b0d448f6c8ec4749e6cc50687006489eb
sokolowski01 <97532@student.wroclaw.merito.pl>
2025-12-07 15:47:59 +0100

    Przykladowe wywolanie sredniej wazonej

--- avg.php
+++ avg.php
@@ -6,14 +6,22 @@ function srednia($liczby) {
 	}
 	return array_sum($liczby) / count($liczby);
 }
-
+function srednia_wazona($wartosci, $wagi) {
+	if (count($wartosci) === 0 || count ($wartosci) !== count ($wagi)) {
+		return 0;
+}
 	$suma_wazona = 0;
 	$suma_wag = 0;
-
+	}
 	for ($i = 0; $i < count($wartosci); $i++) {
-		$suma_wazona += $wartosci[&i] * $wagi[$i];
+		$suma_wazona += $wartosci[$i] * $wagi[$i];
 		$suma_wag += $wagi[$i];
 }
 
 	return $suma_wag === 0 ? 0 : $suma_wazona / $suma_wag;
-}
+
+	}
+	$wartosci = [2, 4, 6];
+	$wagi = [7, 3];
+	
+	echo srednia_wazona(&wartosci, &wagi);
